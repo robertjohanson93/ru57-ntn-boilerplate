84685488454ef35a303360a035c222c9ba18abf4
Robert <robert.johanson.93@gmail.com>
2022-03-20 20:47:21 +0100

    Added icon to button on start page

--- pages/index.vue
+++ pages/index.vue
@@ -2446,9 +2446,13 @@
       </div>
       <a
         href="/teams"
-        class="bg-transparent hover:bg-primary-900 text-primary-050 font-semibold uppercase py-2 px-4 border border-primary hover:border-primary rounded-full inline-flex items-center mt-8"
+        class="button bg-transparent hover:bg-primary-900 text-primary-050 font-semibold uppercase py-2 px-4 border border-primary hover:border-primary rounded-full inline-flex items-center mt-8"
         data-v-1263e8ee=""
-        >Till lagen</a
+        ><svg xmlns="http://www.w3.org/2000/svg" height="1rem" viewBox="0 0 24 24" width="1rem" fill="currentColor">
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z" />
+        </svg>
+        Till lagen</a
       >
       <svg
         xmlns="http://www.w3.org/2000/svg"
